4e23140ec2f7461bf0f355bd636766874857085c
Kpyu <ahukpyu@gmail.com>
2016-09-09 10:39:54 +0800

    update

--- app/view/topic-new-view/topic-new-view.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class TopicNewViewComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-}
--- dist/app/view/topic-new-view/index.d.ts
+++
@@ -1,1 +0,0 @@
-export * from './topic-new-view.component';
