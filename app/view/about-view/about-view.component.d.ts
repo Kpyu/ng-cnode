300d89a57ac4eaec269f931726fe5c1dcf20264c
Kpyu <ahukpyu@gmail.com>
2017-02-08 15:25:08 +0800

    update

--- app/view/about-view/about-view.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class AboutViewComponent implements OnInit {
-    title: string;
-    constructor();
-    ngOnInit(): void;
-}
--- app/view/topic-view/reply-list/reply-list.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class ReplyListComponent implements OnInit {
-    replyList: Array<any>;
-    constructor();
-    ngOnInit(): void;
-}
